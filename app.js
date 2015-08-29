6c22b2bbcb6469f44c7eacdb186c67645f04347f
Samuel Vrablik <samo@vrablik.eu>
2015-08-30 00:59:29 +0200

    .gitignore and some other stuff

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 var menubar = require('menubar')
 var shell = require("shell")
-var Positioner = require('electron-positioner')
 
 var mb = menubar()
 
 
 mb.on('ready', function ready () {
   console.log('app is ready')
-
-
-
-
-  // your app code here
 })
 
 mb.once('show', function () {
-
-  //mb.window.openDevTools();
 
 })
